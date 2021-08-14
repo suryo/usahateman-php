20a7b874fa919cc296c511775c6979a6063a08a9
suryo <suryoatm@gmail.com>
2021-08-14 21:42:03 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+  $host = "localhost"; 
+  $user = "root";
+  $pass = "";
+  $nama_db = "db_usahateman"; //nama database
+  $koneksi = mysqli_connect($host,$user,$pass,$nama_db); //pastikan urutan nya seperti ini, jangan tertukar
+
+  if(!$koneksi){ //jika tidak terkoneksi maka akan tampil error
+    die ("Koneksi dengan database gagal: ".mysql_connect_error());
+  }
+ 
+?>
